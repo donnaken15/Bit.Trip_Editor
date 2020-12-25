9bfd9c28266bef3c59afff54f14cc98a1bab52e1
donnaken15 <wesley@gmx.it>
2020-12-25 05:03:03 -0500

    Update main.cs

--- src/proj/lvbrowse/main.cs
+++ src/proj/lvbrowse/main.cs
@@ -2,10 +2,10 @@
 using System.Windows.Forms;
 using System.Xml;
 using Ionic.Zip;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System;
+using System.Threading;
 
 namespace lvbrowse
 {
@@ -16,9 +16,12 @@ namespace lvbrowse
         XmlDocument DB;
         string[] PlayButtons;
         string[] MediaLinks;
+        string[] LevelEPoints;
         DataGridViewRow newRow;
         Random random = new Random();
         string randId;
+        Process game;// lul just use process.start "pro"
+        char bs = '\\'; // the BS button, activate the BS
 
         public main()
         {
@@ -28,14 +31,23 @@ namespace lvbrowse
 
         public void refresh()
         {
+            loadText.Text = "Initializing process controller...";
+            game = new Process();
+            game.StartInfo = new ProcessStartInfo()
+            {
+                UseShellExecute = true
+            };
             levellist.Rows.Clear();
             loadText.Text = "Downloading database...";
             DB = new XmlDocument();
             DB.Load("https://raw.githubusercontent.com/donnaken15/Bit.Trip_Editor/levels/Community.xml");
+            //local test V
+            //DB.Load("https://localhost/test.xml");
             loadText.Text = "Creating list...";
             levels = DB.GetElementsByTagName("level");
             PlayButtons = new string[levels.Count];
             MediaLinks = new string[levels.Count];
+            LevelEPoints = new string[levels.Count];
             for (int i = 0; i < levels.Count; i++)
             {
                 loadText.Text = "Creating list...("+i.ToString()+
@@ -53,6 +65,7 @@ namespace lvbrowse
                 levellist.Rows.Add(newRow);
                 PlayButtons[i] = levels[i].ChildNodes[4].Attributes[0].Value;
                 MediaLinks[i] = levels[i].ChildNodes[5].Attributes[0].Value;
+                LevelEPoints[i] = levels[i].ChildNodes[4].ChildNodes[0].InnerText;
             }
             loadText.Text = "Ready";
         }
@@ -64,15 +77,27 @@ namespace lvbrowse
                 case 4:
                     randId = Convert.ToString(random.Next(),16);
                     string tmplevel = Path.GetTempPath() + "__BITTRIP_" + randId;
-                    MessageBox.Show(PlayButtons[e.RowIndex]);
                     WC.Proxy = null;
                     WC.Headers.Add("user-agent", "Anything");
+                    //MessageBox.Show(LevelEPoints[e.RowIndex]);
                     ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
                     WC.DownloadFile(PlayButtons[e.RowIndex], tmplevel+".ZIP");
                     Directory.CreateDirectory(tmplevel);
                     using (ZipFile zipFile = new ZipFile(tmplevel+".ZIP"))
                         zipFile.ExtractAll(tmplevel, ExtractExistingFileAction.OverwriteSilently);
-                    Process.Start(tmplevel + "\\Boxxle.btb");
+                    game.StartInfo.FileName = tmplevel + bs + LevelEPoints[e.RowIndex];
+                    loadText.Text = "Starting...";
+                    game.Start();
+                    for (int i = 0; i < 60; i++)
+                        Thread.Sleep(40);
+                    loadText.Text = "Waiting for player to finish...";
+                    while (!game.HasExited)
+                    {
+                        Thread.Sleep(40);
+                        //PerformLayout();
+                        //Update();
+                    }
+                    loadText.Text = "Ready";
                     //File.Delete(tmplevel + ".ZIP");
                     break;
                 case 5:
